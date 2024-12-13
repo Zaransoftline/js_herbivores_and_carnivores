bbeaff4a9aea5eb95fd31009f6487fd9cc3f33a4
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2024-12-12 19:17:12 -0500

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimal(animal) {
+    const index = Animal.alive.indexOf(animal);
+
+    if (index > -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  decreaseHealth(amount) {
+    this.health -= amount;
+
+    if (this.health <= 0) {
+      this.health = 0;
+      Animal.removeDeadAnimal(this);
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (prey.hidden || prey instanceof Carnivore || prey.health <= 0) {
+      return;
+    }
+    prey.decreaseHealth(50);
+  }
 }
 
 module.exports = {
